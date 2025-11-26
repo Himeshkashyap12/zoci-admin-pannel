92d7578868733b25f06cf512b4257c51dc14bf77
himesh kashyap <himesh.kashyap@celestialitverse.com>
2025-11-26 11:13:10 +0530

    update

---
+++ src/component/loader/ImageLoader.jsx
@@ -0,0 +1,8 @@
+ const   ImageLoader=()=> {
+return (
+<div className={`flex flex-col items-center justify-center !h-[200px] gap-4 bg-gray-50`}>
+<div className="w-16 h-16 border-4 border-gray-300 border-t-gray-700 rounded-full animate-spin"></div>
+</div>
+);
+}
+export default ImageLoader;
